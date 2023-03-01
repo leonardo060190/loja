0b35d855d89aafa2f987f7154c81f5e1a0ff6658
leonardo060190 <leonardo-morruga@hotmail.com>
2023-03-01 16:35:43 -0300

    Javascript e html,  Corrido.

--- javascript/index.js
+++ javascript/index.js
@@ -1,21 +1,17 @@
-// login e senha
-const login = "leonardo@hotmail.com"
-const pass = "leonardo"
 
-// Validação de login
-const botao = document.getElementById("entrar");
+function logar(){
+    // login e senha
+    const login = "leonardo@hotmail.com"
+    const pass = "leonardo"
 
-botao.addEventListener("click", function logar(){
     let pegaUsuario = document.getElementById("email").value;
     let pegaSenha = document.getElementById("senha").value;
 
     if (pegaUsuario == login && pegaSenha == pass){
-
-        // Se for valido entra na pagina a baixo
-        location.href='Lista_de_Produtos.html'
-        
+         return true;
         // alerta se a senha e o login incorretos
     }else{
-        alert("email e senha incorretos");
-}
-});
+        alert("email ou senha incorretos! Tente novamente.");
+            return false;
+};
+};
