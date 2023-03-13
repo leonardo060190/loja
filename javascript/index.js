8ab07f82e9a2c95e9c45545a4d7753bf847f7946
leonardo060190 <leonardo-morruga@hotmail.com>
2023-03-13 16:25:04 -0300

    Alterado o html , js e css.

--- javascript/index.js
+++ javascript/index.js
@@ -33,9 +33,10 @@ function cadastrarProdutos(){
         produtos = JSON.parse(sessionStorage.getItem("vetor_produtos"));
 
     };
+    
 
     let descricao_do_Produto = {};
-            descricao_do_Produto ={
+            descricao_do_Produto = {
                 Codigo: codigo,
                 Nome: nome,
                 Categoria: categoria, 
@@ -102,11 +103,79 @@ function listarProdutos(){
 function cadastrar_clientes(){
     let clientes = [];
 
-    let codigo = document.getElementById("incodigo").value;
+    let codigo = document.getElementById("inCodigo").value;
     let nome = document.getElementById("NomeCliente").value;
+    let cpf = document.getElementById("inCPF").value;
+    let dataNacimento = document.getElementById("inData_Nascimento").value;
+    let telefone = document.getElementById("inTelefone").value;
+    let email = document.getElementById("inEmail").value;
 
-}
+        if(sessionStorage.getItem("vetor_Clientes")){
+            clientes = JSON.parse(sessionStorage.getItem("vetor_Clientes"));
+
+        };
+
+        let descricao_do_Cliente = {};
+                descricao_do_Cliente = {
+                    Codigo: codigo,
+                    Nome: nome,
+                    CPF: cpf,
+                    Data_de_Nascimento: dataNacimento,
+                    Telefone: telefone,
+                    Email: email
+
+                };
+
+            clientes.push(descricao_do_Cliente);
+            sessionStorage.setItem("vetor_Clientes", JSON.stringify(clientes));
+
+                return true;
+
+};
 
 function listarClientes(){
+
+    let dados = document.getElementById("colunas");
+    let registros = document.getElementsByTagName("tbody")[0];
+    let clientes = JSON.parse(sessionStorage.getItem("vetor_Clientes"));
+
+    for (let i = 0; i < clientes.length; i++){
+
+        let novaLinha = document.createElement("tr");
+
+        registros.appendChild(novaLinha);
+
+        novaLinha.innerHTML = dados.innerHTML;
+
+        for(let indice in novaLinha.childNodes){
+
+            let celula = novaLinha.childNodes[indice];
+
+            if(celula.nodeName == "TD"){
+
+                switch(celula.dataset.column){
+                    case "Codigo":
+                        celula.innerHTML = clientes[i]["Codigo"];
+                        break;
+                    case "Nome":
+                        celula.innerHTML = clientes[i]["Nome"];
+                        break;
+                    case "CPF":
+                        celula.innerHTML  = clientes[i]["CPF"]
+                        break;
+                    case "Data_de_Nascimento":
+                        celula.innerHTML = clientes[i]["Data_de_Nascimento"];
+                        break;
+                    case "Telefone":
+                        celula.innerHTML = clientes[i]["Telefone"];
+                        break;
+                    case "Email":
+                        celula.innerHTML = clientes[i]["Email"];
+                        break;
+                };
+            };
+        }
+    }
+    
 
 }
